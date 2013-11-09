d8127f6e466e3317a731450f6b96ba3f3c5747fb
Moses Palmér <mosespalmer@gmail.com>
2013-11-09 18:01:07 +0100

    Added dummy module for platform drivers

---
+++ lib/virtualtouchpad/osevent/__init__.py
@@ -0,0 +1,64 @@
+# coding=utf-8
+'''
+virtual-touchpad
+Copyright (C) 2013 Moses Palmér
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+
+def _import_symbols():
+    """
+    Loads the platform dependent driver and populates the module globals.
+    """
+    import importlib
+    import inspect
+    import sys
+
+    # Get the name of the platform and load the driver module
+    platform = ''.join(c for c in sys.platform if c.isalpha())
+    driver = importlib.import_module(
+        '._%s' % platform,
+        __package__)
+
+    # Get symbols exported from the driver
+    symbols = {}
+    for name in dir(driver):
+        value = getattr(driver, name)
+
+        # We ignore all private symbols
+        if name[0] == '_':
+            continue
+
+        # The symbol must exist as a global callable in this module
+        old_value = globals().get(name, None)
+        if old_value is None:
+            continue
+        if not callable(old_value):
+            raise ImportError(
+                'error in %s: symbol <%s> must not be global',
+                driver.__name__, name)
+
+        # If the symbol is callable, the function signatures must match
+        if callable(value):
+            argspec = inspect.getargspec(value)
+            old_argspec = inspect.getargspec(old_value)
+            if argspec != old_argspec:
+                raise ImportError(
+                    'error in %s: invalid method signature for <%s>',
+                    driver.__name__, name)
+
+        # Replace the global
+        value.__doc__ = old_value.__doc__
+        globals()[name] = value
+
+_import_symbols()

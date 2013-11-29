44143c401b2b9ba31011be88944cc4732def38c3
Moses Palmér <mosespalmer@gmail.com>
2013-11-29 18:14:36 +0100

    Added function to check whether specific test failed

--- res/checks.js
+++ res/checks.js
@@ -2,5 +2,18 @@ exports.checks = (function() {
     var module = {
         failures: []};
 
+    /**
+     * Determines whether a check has failed.
+     *
+     * A check that has not been performed has not failed.
+     *
+     * @param check
+     *     The name of the check.
+     * @return whether the check failed
+     */
+    module.failed = function(check) {
+        return module.failures.indexOf(check) != -1;
+    }
+
     return module;
 })();

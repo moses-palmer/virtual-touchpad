ac538db9e3d56d4c6be1dbbb081688d1a8d442ef
Moses Palmér <mosespalmer@gmail.com>
2013-11-30 02:15:23 +0100

    Added event handler stubs for Touchview

--- res/view.js
+++ res/view.js
@@ -7,8 +7,45 @@ exports.view = (function() {
     var Touchview = function(parentEl, touchpad) {
         this.parentEl = parentEl;
         this.touchpad = touchpad;
+
+        var self = this;
+        this.parentEl.addEventListener("touchstart", function() {
+            return self.onTouchStart.apply(self, arguments);
+        });
+        this.parentEl.addEventListener("touchend", function() {
+            return self.onTouchEnd.apply(self, arguments);
+        });
+        this.parentEl.addEventListener("touchcancel", function() {
+            return self.onTouchCancel.apply(self, arguments);
+        });
+        this.parentEl.addEventListener("touchleave", function() {
+            return self.onTouchEnd.apply(self, arguments);
+        });
+        this.parentEl.addEventListener("touchmove", function() {
+            return self.onTouchMove.apply(self, arguments);
+        });
     };
     module.Touchview = Touchview;
+
+    Touchview.prototype.onTouchStart = function(event) {
+        // TODO: Implement
+    };
+
+    Touchview.prototype.onTouchEnd = function(event) {
+        // TODO: Implement
+    };
+
+    Touchview.prototype.onTouchCancel = function(event) {
+        // TODO: Implement
+    };
+
+    Touchview.prototype.onTouchLeave = function(event) {
+        // TODO: Implement
+    };
+
+    Touchview.prototype.onTouchMove = function(event) {
+        // TODO: Implement
+    };
 
     return module;
 })();

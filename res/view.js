3f401383d95877872c9393bde3d65e1f4763081b
Moses Palmér <mosespalmer@gmail.com>
2013-11-30 02:07:32 +0100

    Added empty Touchview class

--- res/view.js
+++ res/view.js
@@ -1,5 +1,14 @@
 exports.view = (function() {
     var module = {};
 
+    /**
+     * The touchpad view.
+     */
+    var Touchview = function(parentEl, touchpad) {
+        this.parentEl = parentEl;
+        this.touchpad = touchpad;
+    };
+    module.Touchview = Touchview;
+
     return module;
 })();

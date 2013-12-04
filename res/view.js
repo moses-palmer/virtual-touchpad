828e14f540857dca1d0c252efe3a3181f204d817
Moses Palmér <mosespalmer@gmail.com>
2013-12-04 20:53:46 +0100

    Added function to clone TouchList

--- res/view.js
+++ res/view.js
@@ -27,6 +27,28 @@ exports.view = (function() {
     };
     module.Touchview = Touchview;
 
+    /**
+     * Clones an array of touches keeping only identifier, screenX and screenY.
+     *
+     * @param touches
+     *     A TouchList to clone. If this is falsy, [] is returned.
+     * @return an array of objects
+     */
+    function cloneTouches(touches) {
+        if (!touches) return [];
+
+        var result = [];
+
+        for (var i = 0; i < touches.length; i++) {
+            result.push({
+                identifier: touches[i].identifier,
+                screenX: touches[i].screenX,
+                screenY: touches[i].screenY});
+        }
+
+        return result;
+    }
+
     Touchview.prototype.onTouchStart = function(event) {
         // TODO: Implement
     };

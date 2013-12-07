d16adcbe24b138ff2b64dd20d6c8a54e6f017967
Moses Palmér <mosespalmer@gmail.com>
2013-12-08 00:29:02 +0100

    Read package information from virtualtouchpad._info

--- setup.py
+++ setup.py
@@ -7,8 +7,25 @@ import sys
 from setuptools import setup, find_packages
 
 
+# Read globals from virtualtouchpad._info without loading it
+info = {}
+with open(os.path.join(
+        os.path.dirname(__file__),
+        'lib',
+        'virtualtouchpad',
+        '_info.py')) as f:
+    for line in f:
+        try:
+            name, value = (i.strip() for i in line.split('='))
+            if name.startswith('__') and name.endswith('__'):
+                info[name[2:-2]] = eval(value)
+        except ValueError:
+            pass
+
+
 setup(
     name = 'virtual-touchpad',
+    version = '.'.join(str(i) for i in info['version']),
     description = ''
         'Turns your mobile or tablet into a touchpad and keyboard for your '
         'computer.',
@@ -18,6 +35,7 @@ setup(
         'gevent >=0.13',
         'gevent-websocket >=0.9'],
 
+    author = info['author'],
     author_email = 'moses.palmer@gmail.com',
 
     url = 'https://github.com/moses-palmer/virtual-touchpad',

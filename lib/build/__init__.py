fdcb365bbc068097f0f746f1ddb84578faa8f414
Moses Palmér <mosespalmer@gmail.com>
2013-12-17 20:31:33 +0100

    Moved platform package detection to separate file

--- lib/build/__init__.py
+++ lib/build/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 # Read globals from virtualtouchpad._info without loading it
 info = {}
@@ -23,3 +24,29 @@ with open(os.path.join(
         os.pardir,
         'README')) as f:
     README = f.read()
+
+
+def platform_requirements():
+    """
+    A list of PyPi packages that are dependencies only for the current platform.
+    """
+    platform = ''.join(c for c in sys.platform if c.isalpha())
+    result = []
+
+    # We only support linux
+    if platform == 'linux':
+        if sys.version_info.major == 3:
+            result.append('python3-xlib')
+        elif sys.version_info.major == 2:
+            result.append('python-xlib')
+        else:
+            raise NotImplementedError(
+                'This python major version (%d) is not supported',
+                sys.version_info.major)
+
+    else:
+        raise NotImplementedError(
+            'This platform (%s) is not supported',
+            sys.platform)
+
+    return result

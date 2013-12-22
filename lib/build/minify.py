8d3802cb0b63bd0b0bc1dbff0043639c0effe65f
Moses Palmér <mosespalmer@gmail.com>
2013-12-22 16:18:04 +0100

    Minify by inlining script tags

--- lib/build/minify.py
+++ lib/build/minify.py
@@ -1,6 +1,7 @@
 import os
 
 from xml.dom import Node
+from xml.dom.minidom import CDATASection
 
 
 def _trim(e):
@@ -14,6 +15,27 @@ def _remove_comments(e):
     """Removes comment nodes"""
     if e.nodeType == Node.COMMENT_NODE and e.parentNode:
         e.parentNode.removeChild(e).unlink()
+
+
+def _inline_script(e, source_dir, dom):
+    """Inlines script tags"""
+    # Only use script tags with src attribute
+    if e.nodeType != Node.ELEMENT_NODE \
+            or e.tagName != 'script' \
+            or not e.hasAttribute('src'):
+        return
+
+    # Read the script source and inline it
+    src_path = os.path.join(
+        source_dir,
+        e.getAttribute('src'))
+    with open(src_path) as src:
+        cdata = CDATASection()
+        cdata.replaceWholeText(src.read())
+        e.appendChild(cdata)
+
+    # Make sure to remove the src attribute
+    e.removeAttribute('src')
 
 
 def html(source_path, target_path):
@@ -39,6 +61,11 @@ def html(source_path, target_path):
 
     # Remove comments
     recurse(dom.documentElement, _remove_comments)
+
+    # Inline script tags
+    recurse(dom.documentElement, _inline_script,
+        source_dir = os.path.dirname(source_path),
+        dom = dom)
 
     # Normalise the XML
     recurse(dom.documentElement,

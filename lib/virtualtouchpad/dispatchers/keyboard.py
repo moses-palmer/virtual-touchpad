de410e2cfd9a5563fa454b26dd9d398ef154a535
Moses Palmér <mosespalmer@gmail.com>
2014-02-23 21:37:34 +0100

    Updated copyright years

--- lib/virtualtouchpad/dispatchers/keyboard.py
+++ lib/virtualtouchpad/dispatchers/keyboard.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 '''
 virtual-touchpad
-Copyright (C) 2013 Moses Palmér
+Copyright (C) 2013-2014 Moses Palmér
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software

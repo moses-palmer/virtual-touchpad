949d64291c573e0e92412486834f5b377d82ba41
Moses Palmér <mosespalmer@gmail.com>
2014-02-26 19:51:46 +0100

    Added empty script to make release

---
+++ scripts/make-release.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        try:
+            sys.stderr.write(e.args[0] % e.args[1:] + '\n')
+        except:
+            sys.stderr.write('%s\n' % str(e))
+        sys.exit(1)

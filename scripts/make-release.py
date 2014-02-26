cd34e5163056f136057e2d90d6f1c01065aae594
Moses Palmér <mosespalmer@gmail.com>
2014-02-26 22:04:11 +0100

    Added function to call git

--- scripts/make-release.py
+++ scripts/make-release.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 
+import subprocess
 import sys
+
+
+def git(*args):
+    """
+    Executes git with the command line arguments given.
+
+    @param args...
+        The arguments to git.
+    @return stdout of git
+    @raise RuntimeError if git returns non-zero
+    """
+    g = subprocess.Popen(['git'] + list(args),
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE)
+
+    stdout, stderr = g.communicate()
+    if g.returncode != 0:
+        raise RuntimeError('Failed to call git %s (%d): %s',
+            ' '.join(args),
+            g.returncode, stderr)
+    else:
+        return stdout
 
 
 def main():

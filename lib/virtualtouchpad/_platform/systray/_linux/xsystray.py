e3850b48e531eb8b9ab14323f18eec0565c34d77
Moses Palmér <mosespalmer@gmail.com>
2014-03-16 22:00:45 +0100

    Added display property to XSystemTrayIcon

--- lib/virtualtouchpad/_platform/systray/_linux/xsystray.py
+++ lib/virtualtouchpad/_platform/systray/_linux/xsystray.py
@@ -28,7 +28,29 @@ class XSystemTrayIcon(object):
             A descriptive text to apply to the icon.
         """
         self._description = description
-        # TODO: Implement
+        self._display = None
+
+    @property
+    def display(self):
+        """The X display as a context manager that will collect X errors on exit
+        and raise a RuntimeError if any errors occurred"""
+        # Make sure we have a Display instance
+        if self._display is None:
+            raise RuntimeError('display not initiated')
+
+        from contextlib import contextmanager
+
+        @contextmanager
+        def manager():
+            errors = []
+            old_handler = self._display.set_error_handler(errors.append)
+            yield self._display
+            self._display.sync()
+            self._display.set_error_handler(old_handler)
+            if errors:
+                raise RuntimeError(errors)
+
+        return manager()
 
     def destroy(self):
         """

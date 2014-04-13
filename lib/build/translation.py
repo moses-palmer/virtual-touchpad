4880cfbcabb5a9131cff2aeb554ca5695afce13c
Moses Palmér <mosespalmer@gmail.com>
2014-04-13 17:34:29 +0200

    Monkey-patch XML parser to store file location in node

--- lib/build/translation.py
+++ lib/build/translation.py
@@ -1,4 +1,5 @@
 from xml.dom.minidom import parse
+from xml.sax import make_parser
 
 
 def read_translatable_strings(path):
@@ -9,6 +10,23 @@ def read_translatable_strings(path):
         The path to the file.
     @return a polib.POFile instance
     """
-    dom = parse(path)
+    def set_content_handler(dom_handler):
+        def start_element_ns(name, tagName , attrs):
+            orig_start_cb(name, tagName, attrs)
+            cur_elem = dom_handler.elementStack[-1]
+            cur_elem.parse_position = (
+                parser._parser.CurrentLineNumber,
+                parser._parser.CurrentColumnNumber)
+
+        orig_start_cb = dom_handler.startElementNS
+        dom_handler.startElementNS = start_element_ns
+        orig_set_content_handler(dom_handler)
+
+    # Monkey-patch the parser to store the original location in each node
+    parser = make_parser()
+    orig_set_content_handler = parser.setContentHandler
+    parser.setContentHandler = set_content_handler
+
+    dom = parse(path, parser)
 
     # TODO: Implement

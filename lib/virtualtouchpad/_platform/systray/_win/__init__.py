f477b33ddb3ec0a4467c50bb699a80a2bf87c108
Moses Palmér <mosespalmer@gmail.com>
2014-04-22 21:26:25 +0200

    Create SystemTrayIcon instead of calling virtualtouchpad._platform.systray.create/destroy

--- lib/virtualtouchpad/_platform/systray/_win/__init__.py
+++ lib/virtualtouchpad/_platform/systray/_win/__init__.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import win32systray
+from win32systray import *
 
 
 def create(description):

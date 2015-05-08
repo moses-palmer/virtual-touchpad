a7b44f38768a9a88f1a45ea67da9904fac1f6b00
Moses Palmér <mosespalmer@gmail.com>
2015-05-08 22:56:22 +0200

    Implemented keyboard handling for win32

---
+++ lib/virtualtouchpad/event/_win32_syms.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+# virtual-touchpad
+# Copyright (C) 2013-2015 Moses Palmér
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+#: A mapping from X symbol name to win32 Virtual Key Codes
+SYMS = {
+    'BackSpace': 0x08,
+    'Return': 0x0D,
+    'space': 0x20,
+    'Tab': 0x09,
+
+    'Menu': 0x5D,
+    'Super_L': 0x5B,
+    'Super_R': 0x5C,
+
+    'Alt_L': 0xA4,
+    'Caps_Lock': 0x14,
+    'Control_L': 0xA2,
+    'Control_R': 0xA3,
+    'ISO_Level3_Shift': 0xA5,
+    'Shift_L': 0xA0,
+    'Shift_R': 0xA1,
+
+    'Escape': 0x1B,
+
+    'F1': 0x70,
+    'F2': 0x71,
+    'F3': 0x72,
+    'F4': 0x73,
+    'F5': 0x74,
+    'F6': 0x75,
+    'F7': 0x76,
+    'F8': 0x77,
+    'F9': 0x78,
+    'F10': 0x79,
+    'F11': 0x7A,
+    'F12': 0x7B,
+
+    'Down': 0x28,
+    'Left': 0x25,
+    'Right': 0x27,
+    'Up': 0x26,
+
+    'End': 0x23,
+    'Home': 0x24,
+    'Next': 0x22,
+    'Prior': 0x21
+}
--- lib/virtualtouchpad/event/win32.py
+++ lib/virtualtouchpad/event/win32.py
@@ -19,9 +19,19 @@ with platform.modules():
     import win32api
 
 import ctypes
+import logging
+import unicodedata
+
+from ._win32_syms import SYMS
+
+
+log = logging.getLogger(__name__)
 
 
 _SendInput = ctypes.windll.user32.SendInput
+
+#: The last dead key; TODO: Set this per connection
+dead_key = None
 
 
 class MOUSEINPUT(ctypes.Structure):
@@ -49,6 +59,11 @@ class MOUSEINPUT(ctypes.Structure):
 
 
 class KEYBDINPUT(ctypes.Structure):
+    EXTENDEDKEY = 0x0001
+    KEYUP = 0x0002
+    SCANCODE = 0x0008
+    UNICODE = 0x0004
+
     _fields_ = [
         ('wVk', ctypes.c_uint16),
         ('wScan', ctypes.c_uint16),
@@ -70,12 +85,94 @@ class INPUT(ctypes.Structure):
         ('value', ANYINPUT)]
 
 
+def is_dead(symbol):
+    """Returns whether a key symbol is a dead key.
+
+    :param symbol: The key symbol.
+    :type symbol: str or None
+
+    :return: whether ``symbol`` is a dead key
+    """
+    return symbol and symbol.startswith('dead_')
+
+
+def set_dead_key(name):
+    """Sets the current dead key.
+
+    :param str name: The name of the key. This should be the actual character
+        representing the dead key, such as ``~`` for *TILDE*.
+
+    :raises KeyError: if the dead cannot be converted to a combining character
+    """
+    # TODO: Use a per connection store
+    global dead_key
+
+    if name is None:
+        dead_key = None
+    else:
+        dead_key = (
+            name,
+            unicodedata.lookup('COMBINING ' + unicodedata.name(name)))
+
+
+def key_event(name, symbol, flags):
+    """Sends a keyboard event.
+
+    If the length of ``name`` is ``1``, it is assumed to be a *unicode*
+    character and the corresponding character will be sent.
+
+    Otherwise, symbol is mapped
+    """
+    if name and len(name) == 1:
+        keyboard = KEYBDINPUT(
+            dwFlags = KEYBDINPUT.UNICODE | flags,
+            wScan = ord(name))
+    else:
+        try:
+            vk = SYMS[symbol]
+        except KeyError:
+            log.error('Unknown symbol: %s', symbol)
+            return
+        keyboard = KEYBDINPUT(
+            dwFlags = flags,
+            wVk = vk)
+    _SendInput(1,
+        ctypes.byref(INPUT(
+            type = INPUT.KEYBOARD,
+            value = ANYINPUT(
+                keyboard = keyboard))),
+        ctypes.sizeof(INPUT))
+
+
 def key_down(name, keysym, symbol):
-    raise NotImplementedError();
+    # Do we have a previous dead key? In that case, first try to combine it with
+    # the current key, and send it, and if that fails just send the dead key
+    # alone
+    global dead_key
+
+    if not dead_key is None:
+        previous_name, combining = dead_key
+        set_dead_key(None)
+        if name and len(name) == 1:
+            combined = unicodedata.normalize('NFC', name + combining)
+            if len(combined) == 1:
+                key_event(combined, None, 0)
+                return
+        key_event(previous_name, None, 0)
+
+    # If the current key is a dead key, save it for later
+    if is_dead(symbol):
+        try:
+            set_dead_key(name)
+            return
+        except Exception as e:
+            log.error('Failed to set dead key: %s', str(e))
+
+    key_event(name, symbol, 0)
 
 
 def key_up(name, keysym, symbol):
-    raise NotImplementedError();
+    key_event(name, symbol, KEYBDINPUT.KEYUP)
 
 
 def mouse_down(button):

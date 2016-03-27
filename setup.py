2bb4ed940cd6511664415256fac3a39d3efa9c69
Moses Palmér <mosespalmer@gmail.com>
2016-03-27 15:44:30 +0200

    Use pystray instead of in-project systray implementation

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ REQUIREMENTS = [
     'gevent >=0.13',
     'gevent-websocket >=0.9',
     'netifaces >=0.8',
+    'Pillow >=1.1.7',
     'pynput >=1.0',
     'pystray >=0.2',
     'zeroconf >=0.17']

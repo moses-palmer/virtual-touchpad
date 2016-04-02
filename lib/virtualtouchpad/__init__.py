fca2cfbfcbe6a0c1e12c9f8f845bcf8e60ca365d
Moses Palmér <mosespalmer@gmail.com>
2016-04-02 17:10:01 +0200

    Moved server main function back to __init__

--- lib/virtualtouchpad/__init__.py
+++ lib/virtualtouchpad/__init__.py
@@ -15,5 +15,25 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
+from .routes import app
 
-from ._info import *
+
+def server(port, address):
+    """Creates the actual server instance.
+
+    :param int port: The port on which to listen.
+
+    :param address: The address on which to listen.
+
+    :return: a server instance
+    """
+    import gevent.monkey
+    import gevent.pywsgi
+    import geventwebsocket.handler
+    import virtualtouchpad.routes
+
+    gevent.monkey.patch_all(thread=False)
+    return gevent.pywsgi.WSGIServer(
+        ('0.0.0.0', port),
+        app,
+        handler_class=geventwebsocket.handler.WebSocketHandler)

3003e5e0c097d3a02ae810d804d333c7b2df451f
Moses Palmér <mosespalmer@gmail.com>
2016-04-03 12:21:53 +0200

    Moved @get and @websocket to bottle specific file

--- lib/virtualtouchpad/routes/__init__.py
+++ lib/virtualtouchpad/routes/__init__.py
@@ -15,16 +15,10 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
-import functools
-import json
-import logging
-import traceback
-import types
+import sys
 
-import bottle
-import geventwebsocket
-
-from virtualtouchpad._server_bottle import app
+if sys.version_info.major < 3:
+    from ._routes_bottle import get, websocket
 
 
 class HTTPResponse(object):
@@ -34,99 +28,6 @@ class HTTPResponse(object):
         self.status = status
         self.body = body or ''
         self.headers = headers or {}
-
-
-def get(path):
-    """A decorator to mark a function as handling an *HTTP* ``GET`` request.
-
-    Please see :func:`bottle.Application.get` for a reference on the format of
-    ``path``.
-
-    The decorated function is passed the request headers, where header names are
-    lower case, as its first parameter, and any values extracted from the path.
-
-    :param str path: The path this function handles.
-    """
-    log = logging.getLogger('%s.%s' % (__name__, path))
-
-    def inner(handler):
-        @app.get(path)
-        def wrapper(*args, **kwargs):
-            try:
-                headers = {
-                    key.lower(): value
-                    for key, value in bottle.request.headers.items()}
-                response = handler(headers, *args, **kwargs)
-                if isinstance(response, types.DictionaryType):
-                    response = HTTPResponse(200, body=json.dumps(response))
-                return bottle.HTTPResponse(
-                    body=response.body,
-                    status=response.status,
-                    headers=response.headers)
-
-            except Exception as e:
-                log.exception('An error occurred when handling request')
-                raise bottle.HTTPError(status=500)
-
-        return handler
-
-    return inner
-
-
-def websocket(path):
-    """A decorator to mark a function as handling incoming *WebSocket* commands.
-
-    This is not a generic *WebSocket* handler; it will only handle incoming
-    data.
-
-    The decorated function must be a generator. It will be sent ``None``
-    followed by any data received.
-
-    :param str path: The path this function handles.
-    """
-    log = logging.getLogger('%s.%s' % (__name__, path))
-
-    def inner(handler):
-        @app.route(path)
-        def wrapper():
-            # Get the actual websocket
-            ws = bottle.request.environ.get('wsgi.websocket')
-            log.info(
-                'WebSocket with %s opened',
-                bottle.request.environ.get('REMOTE_ADDR'))
-            if not ws:
-                bottle.abort(400, 'Expected WebSocket request.')
-
-            def report_error(reason, exception, tb):
-                ws.send(json.dumps(dict(
-                    reason=reason,
-                    exception=type(exception).__name__,
-                    data=str(exception),
-                    tb=traceback.extract_tb(tb))))
-
-            dispatcher = handler(report_error)
-            next(dispatcher)
-
-            while True:
-                try:
-                    message = ws.receive()
-                    if message is None:
-                        break
-                    dispatcher.send(message)
-
-                except geventwebsocket.WebSocketError:
-                    log.exception('Failed to read WebSocket data')
-                    break
-
-                except Exception as e:
-                    log.exception(
-                        'An error occurred while dispatching %s',
-                        message)
-                    break
-
-        return handler
-
-    return inner
 
 
 # Importing these modules will attach routes to app

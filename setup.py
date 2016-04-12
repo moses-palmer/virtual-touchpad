beeb715b7044dc0e8f3949a528c27aff4e396262
Moses Palmér <mosespalmer@gmail.com>
2016-04-12 12:04:57 +0200

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf8
 
+import distutils
+import distutils.command.build
 import os
 import setuptools
 
@@ -11,19 +13,6 @@ sys.path.append(os.path.join(
     'lib'))
 import _build as buildlib
 
-
-# Data for the package; this will not be evaluated until the build steps have
-# completed
-PACKAGE_DATA = {
-    'virtualtouchpad': [
-        'html/*.*',
-        'html/css/*.*',
-        'html/img/*.*',
-        'html/js/*.*',
-        'html/js/*/*.*',
-        'html/keyboard/*.*',
-        'html/keyboard/*/*.*',
-        'html/translations/*/*.*']}
 
 REQUIREMENTS = [
     'netifaces >=0.8',
@@ -49,6 +38,20 @@ EXTRA_PACKAGES = {
 # The directories in which the packages can be found
 PACKAGE_DIR = {
     'virtualtouchpad': 'lib/virtualtouchpad'}
+
+# Data for the package; this will not be evaluated until the build steps have
+# completed
+PACKAGE_DATA = {
+    'virtualtouchpad': [
+        'html/*.*',
+        'html/css/*.*',
+        'html/img/*.*',
+        'html/js/*.*',
+        'html/js/*/*.*',
+        'html/keyboard/*.*',
+        'html/keyboard/*/*.*',
+        'html/translations/*/*.*']}
+
 
 # These are the arguments passed to setuptools.setup; they are further modified
 # below
@@ -101,26 +104,203 @@ def build_command(cls):
     return cls
 
 
-def build_helper(cls):
-    """Registers a class as a helper command to run when invoking ``build``.
-
-    :param cls: The command class.
+class Command(setuptools.Command):
+    """Convenience class to avoid having to define all required fields.
     """
-    import distutils.command.build
-
-    distutils.command.build.build.sub_commands.append((cls.__name__, None))
-
-    return build_command(cls)
+    user_options = []
+    def initialize_options(self): pass
+    def finalize_options(self): pass
+    def run(self):
+        for cmd_name in self.get_sub_commands():
+            self.run_command(cmd_name)
 
 
 @build_command
-class xgettext(setuptools.Command):
+class build(distutils.command.build.build):
+    sub_commands = list(distutils.command.build.build.sub_commands) + [
+        ('generate_res', None)]
+
+
+@build_command
+class generate_res(Command):
+    description = 'generates all resources'
+    sub_commands = [
+        ('minify_html', None),
+        ('generate_icons', None),
+        ('generate_translations', None)]
+
+
+@build_command
+class minify_html(Command):
+    files = (
+        ('index.xhtml', True),
+        ('help/index.xhtml', False))
+    description = 'minify html files'
+
+    def minify(self, name, include_appcache):
+        dom_context = buildlib.xmltransform.start(
+            os.path.join(
+                buildlib.HTML_ROOT,
+                name))
+
+        buildlib.xmltransform.minify_html(dom_context)
+
+        if include_appcache:
+            buildlib.xmltransform.add_manifest(
+                dom_context,
+                'virtual-touchpad.appcache')
+
+        base, ext = name.rsplit('.', 1)
+        buildlib.xmltransform.end(
+            dom_context,
+            os.path.join(
+                buildlib.HTML_ROOT,
+                base + '.min.' + ext))
+
+    def run(self):
+        for name, include_appcache in self.files:
+            self.minify(name, include_appcache)
+
+
+@build_command
+class generate_icons(Command):
+    description = 'generates all icons'
+    sub_commands = [
+        ('generate_webapp_icons', None),
+        ('generate_favicon', None)]
+
+
+@build_command
+class generate_webapp_icons(Command):
+    description = 'generate web application icons from SVG sources'
+
+    # The icon dimensions to generate
+    DIMENSIONS = (196, 144, 114, 96, 72, 57, 48)
+
+    def run(self):
+        # Generate the application icons
+        for size in self.DIMENSIONS:
+            buildlib.icons.app_icon(
+                size,
+                os.path.join(
+                    buildlib.HTML_ROOT,
+                    'img',
+                    'icon%dx%d.png' % (size, size)))
+
+
+@build_command
+class generate_favicon(Command):
+    description = 'generate a favicon from SVG sources'
+
+    # The icon dimensions to generate
+    DIMENSIONS = (128, 64, 32, 16)
+
+    def run(self):
+        target_dir = os.path.join(
+            os.path.dirname(__file__),
+            'build',
+            'icos')
+        if not os.path.isdir(target_dir):
+            os.makedirs(target_dir)
+
+        # Generate the application icons
+        for size in self.DIMENSIONS:
+            buildlib.icons.app_icon(
+                size,
+                os.path.join(
+                    target_dir,
+                    'icon%dx%d.ico' % (size, size)))
+        # Create the composite icon
+        buildlib.icons.combine(
+            os.path.join(
+                    buildlib.HTML_ROOT,
+                    'favicon.ico'),
+            *(os.path.join(
+                    target_dir,
+                    'icon%dx%d.ico' % (size, size))
+                for size in self.DIMENSIONS))
+
+
+@build_command
+class generate_translations(Command):
+    description = 'generate translation catalogues from PO files'
+
+    def run(self):
+        import json
+
+        source_dir = os.path.join(
+            os.path.dirname(__file__),
+            'po')
+        target_dir = os.path.join(
+            os.path.dirname(__file__),
+            'lib',
+            'virtualtouchpad',
+            'html',
+            'translations')
+
+        for domain in os.listdir(source_dir):
+            domain_path = os.path.join(source_dir, domain)
+            if not os.path.isdir(domain_path):
+                continue
+
+            for language in os.listdir(domain_path):
+                language_path = os.path.join(domain_path, language)
+                if not language_path.endswith('.po'):
+                    continue
+
+                code, catalog = self.generate_catalog(language_path)
+                with open(os.path.join(
+                        target_dir,
+                        domain,
+                        code + '.js'), 'w') as f:
+                    f.write('exports.translation.catalog=')
+                    json.dump(catalog, f)
+
+    def generate_catalog(self, language_path):
+        import polib
+
+        # Load the PO file
+        pofile = polib.pofile(language_path)
+
+        # Extract interesting meta data
+        code = pofile.metadata['Language']
+        plurals = {
+            key.strip(): value.strip()
+            for (key, value) in (
+                keyvalue.split('=', 1)
+                for keyvalue in pofile.metadata[
+                    'Plural-Forms'].split(';')
+                if keyvalue)}
+
+        # Create the catalogue skeleton
+        texts = {}
+        catalog = {}
+        catalog['code'] = code
+        catalog['plural'] = plurals['plural']
+        catalog['texts'] = texts
+
+        # Populate the catalogue from the PO file
+        for entry in pofile:
+            texts[entry.msgid] = self.create_entry(entry, plurals)
+
+        return code, catalog
+
+    def create_entry(self, entry, plurals):
+        # If this is a plural string, we first create a list of empty strings
+        # and then populate it, otherwise we simply return the string
+        if entry.msgid_plural:
+            result = [''] * int(plurals['nplurals'])
+            for n, msgstr in entry.msgstr_plural.items():
+                result[int(n)] = msgstr
+            return result
+
+        else:
+            return entry.msgstr
+
+
+@build_command
+class xgettext(Command):
     description = 'update the POT files'
-    user_options = []
-
-    def initialize_options(self): pass
-
-    def finalize_options(self): pass
 
     def run(self):
         source_dir = buildlib.HTML_ROOT
@@ -162,198 +342,6 @@ class xgettext(setuptools.Command):
                 buildlib.translation.merge_catalogs(potfile, pofile)
 
 
-@build_helper
-class minify_index(setuptools.Command):
-    description = 'minify index.xhtml'
-    user_options = []
-
-    def initialize_options(self): pass
-
-    def finalize_options(self): pass
-
-    def run(self):
-        # Load index.html
-        dom_context = buildlib.xmltransform.start(
-            os.path.join(
-                buildlib.HTML_ROOT,
-                'index.xhtml'))
-
-        # Minify the index file
-        buildlib.xmltransform.minify_html(dom_context)
-
-        # Add the manifest file
-        buildlib.xmltransform.add_manifest(
-            dom_context,
-            'virtual-touchpad.appcache')
-
-        # Write index.min.xhtml
-        buildlib.xmltransform.end(
-            dom_context,
-            os.path.join(
-                buildlib.HTML_ROOT,
-                'index.min.xhtml'))
-
-
-@build_helper
-class minify_help(setuptools.Command):
-    description = 'minify help/index.xhtml'
-    user_options = []
-
-    def initialize_options(self): pass
-
-    def finalize_options(self): pass
-
-    def run(self):
-        # Load help.xhtml
-        dom_context = buildlib.xmltransform.start(
-            os.path.join(
-                buildlib.HTML_ROOT,
-                'help',
-                'index.xhtml'))
-
-        # Minify the index file
-        buildlib.xmltransform.minify_html(dom_context)
-
-        # Write help.min.xhtml
-        buildlib.xmltransform.end(
-            dom_context,
-            os.path.join(
-                buildlib.HTML_ROOT,
-                'help',
-                'index.min.xhtml'))
-
-
-@build_helper
-class generate_webapp_icons(setuptools.Command):
-    description = 'generate web application icons from SVG sources'
-    user_options = []
-
-    def initialize_options(self): pass
-
-    def finalize_options(self): pass
-
-    def run(self):
-        # Generate the application icons
-        for size in (196, 144, 114, 96, 72, 57, 48):
-            buildlib.icons.app_icon(
-                size,
-                os.path.join(
-                    buildlib.HTML_ROOT,
-                    'img',
-                    'icon%dx%d.png' % (size, size)))
-
-
-@build_helper
-class generate_favicon(setuptools.Command):
-    description = 'generate a favicon from SVG sources'
-    user_options = []
-    DIMENSIONS = (128, 64, 32, 16)
-
-    def initialize_options(self): pass
-
-    def finalize_options(self): pass
-
-    def run(self):
-        target_dir = os.path.join(
-            os.path.dirname(__file__),
-            'build',
-            'icos')
-        if not os.path.isdir(target_dir):
-            os.makedirs(target_dir)
-
-        # Generate the application icons
-        for size in self.DIMENSIONS:
-            buildlib.icons.app_icon(
-                size,
-                os.path.join(
-                    target_dir,
-                    'icon%dx%d.ico' % (size, size)))
-        buildlib.icons.combine(
-            os.path.join(
-                    buildlib.HTML_ROOT,
-                    'favicon.ico'),
-            *(os.path.join(
-                    target_dir,
-                    'icon%dx%d.ico' % (size, size))
-                for size in self.DIMENSIONS))
-
-
-@build_helper
-class generate_translations(setuptools.Command):
-    description = 'generate translation catalogues from PO files'
-    user_options = []
-
-    def initialize_options(self): pass
-
-    def finalize_options(self): pass
-
-    def run(self):
-        import json
-        import polib
-
-        source_dir = os.path.join(
-            os.path.dirname(__file__),
-            'po')
-        target_dir = os.path.join(
-            os.path.dirname(__file__),
-            'lib',
-            'virtualtouchpad',
-            'html',
-            'translations')
-
-        for domain in os.listdir(source_dir):
-            # Only handle directories under the source directory
-            domain_path = os.path.join(source_dir, domain)
-            if not os.path.isdir(domain_path):
-                continue
-
-            for language in os.listdir(domain_path):
-                # Load the PO file
-                language_path = os.path.join(domain_path, language)
-                if not language_path.endswith('.po'):
-                    continue
-                pofile = polib.pofile(language_path)
-
-                # Extract interesting meta data
-                code = pofile.metadata['Language']
-                plurals = {
-                    key.strip(): value.strip()
-                    for (key, value) in (
-                        keyvalue.split('=', 1)
-                        for keyvalue in pofile.metadata[
-                            'Plural-Forms'].split(';')
-                        if keyvalue)}
-
-                # Create the catalogue skeleton
-                texts = {}
-                catalog = {}
-                catalog['code'] = code
-                catalog['plural'] = plurals['plural']
-                catalog['texts'] = texts
-
-                # Populate the catalogue from the PO file
-                for entry in pofile:
-                    if entry.msgid_plural:
-                        # If this is a plural string, we first create a list of
-                        # empty strings and then populate it
-                        texts[entry.msgid] = [''] * int(
-                            plurals['nplurals'])
-                        for n, msgstr in entry.msgstr_plural.items():
-                            texts[entry.msgid][int(n)] = msgstr
-
-                    else:
-                        # If this is non-plural string, we simply copy it
-                        texts[entry.msgid] = entry.msgstr
-
-                # Write the catalogue as JavaScript code
-                with open(os.path.join(
-                        target_dir,
-                        domain,
-                        code + '.js'), 'w') as f:
-                    f.write('exports.translation.catalog=')
-                    json.dump(catalog, f)
-
-
 # Read globals from virtualtouchpad._info without loading it
 INFO = {}
 with open(os.path.join(
@@ -378,8 +366,8 @@ def read(name):
         with open(os.path.join(
                 os.path.dirname(__file__),
                 os.pardir,
-                name)) as f:
-            return f.read()
+                name), 'rb') as f:
+            return f.read().decode('utf-8')
     except IOError:
         return ''
 setup_arguments['long_description'] = '\n\n'.join(
@@ -387,4 +375,5 @@ setup_arguments['long_description'] = '\n\n'.join(
     for name in ('README.rst', 'CHANGES.rst'))
 
 
-setuptools.setup(**setup_arguments)
+if __name__ == '__main__':
+    setuptools.setup(**setup_arguments)

470dcc96ac9357e427a93810f6ff8bccf96e1064
Moses Palmér <mosespalmer@gmail.com>
2016-09-27 15:20:36 +0200

    Moved translation generation to separate package

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ from buildlib import ROOT, BUILDDIR, LIBDIR, PDIR
 
 import buildlib.commands.icons as icons
 import buildlib.commands.minify as minify
+import buildlib.commands.translations as translations
 
 
 # Make sure we can import the main package
@@ -96,78 +97,6 @@ setup_arguments = dict(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5'])
-
-
-@build_command('generate translation catalogues from PO files')
-class generate_translations(Command):
-    def run(self):
-        import json
-
-        source_dir = os.path.join(
-            ROOT,
-            'po')
-        target_dir = os.path.join(
-            buildlib.HTML_ROOT,
-            'translations')
-
-        for domain in os.listdir(source_dir):
-            domain_path = os.path.join(source_dir, domain)
-            if not os.path.isdir(domain_path):
-                continue
-
-            for language in os.listdir(domain_path):
-                language_path = os.path.join(domain_path, language)
-                if not language_path.endswith('.po'):
-                    continue
-
-                code, catalog = self.generate_catalog(language_path)
-                with open(os.path.join(
-                        target_dir,
-                        domain,
-                        code + '.js'), 'w') as f:
-                    f.write('exports.translation.catalog=')
-                    json.dump(catalog, f)
-
-    def generate_catalog(self, language_path):
-        import polib
-
-        # Load the PO file
-        pofile = polib.pofile(language_path)
-
-        # Extract interesting meta data
-        code = pofile.metadata['Language']
-        plurals = {
-            key.strip(): value.strip()
-            for (key, value) in (
-                keyvalue.split('=', 1)
-                for keyvalue in pofile.metadata[
-                    'Plural-Forms'].split(';')
-                if keyvalue)}
-
-        # Create the catalogue skeleton
-        texts = {}
-        catalog = {}
-        catalog['code'] = code
-        catalog['plural'] = plurals['plural']
-        catalog['texts'] = texts
-
-        # Populate the catalogue from the PO file
-        for entry in pofile:
-            texts[entry.msgid] = self.create_entry(entry, plurals)
-
-        return code, catalog
-
-    def create_entry(self, entry, plurals):
-        # If this is a plural string, we first create a list of empty strings
-        # and then populate it, otherwise we simply return the string
-        if entry.msgid_plural:
-            result = [''] * int(plurals['nplurals'])
-            for n, msgstr in entry.msgstr_plural.items():
-                result[int(n)] = msgstr
-            return result
-
-        else:
-            return entry.msgstr
 
 
 @build_command('update the POT files')
@@ -281,7 +210,7 @@ class test(setuptools.command.test.test):
 @build_command('generate all resources',
                minify.minify_html,
                icons.generate_icons,
-               generate_translations)
+               translations.generate_translations)
 class generate_res(Command):
     pass
 

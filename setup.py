79b8936561c4246a5f4703bc76764e474e39fc97
Moses Palmér <mosespalmer@gmail.com>
2016-09-27 15:38:11 +0200

    Mode node dependency installation to separate package

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ from buildlib import ROOT, BUILDDIR, LIBDIR, PDIR
 
 import buildlib.commands.icons as icons
 import buildlib.commands.minify as minify
+import buildlib.commands.node as node
 import buildlib.commands.translations as translations
 
 
@@ -141,66 +142,8 @@ class xgettext(Command):
                 buildlib.translation.merge_catalogs(potfile, pofile)
 
 
-@build_command('install node dependencies')
-class node_dependencies(Command):
-    PACKAGE_COMMAND = ['npm', 'install']
-
-    def node(self):
-        """Makes sure that node.js is installed"""
-        sys.stdout.write('Checking node.js installation...\n')
-
-        # Since node.js picked an already used binary name, we must check
-        # whether "node" is node.js or node - Amateur Packet Radio Node program;
-        # The former actually provides output for the --version command
-        try:
-            node_output = subprocess.check_output(['node', '--version']).decode(
-                'utf8')
-            if node_output.strip():
-                sys.stdout.write(node_output)
-                return
-        except OSError:
-            pass
-
-        # On Debian, node is called nodejs because of the above mentioned clash
-        try:
-            subprocess.check_call(['nodejs', '--version'])
-            return
-        except OSError:
-            pass
-
-        sys.stderr.write('node.js is not installed; terminating\n')
-        sys.exit(1)
-
-    def npm(self):
-        """Makes sure that npm is installed"""
-        sys.stdout.write('Checking npm installation...\n')
-
-        try:
-            subprocess.call(['npm', '--version'])
-        except OSError:
-            sys.stderr.write('npm is not installed; terminating\n')
-            sys.exit(1)
-
-    def packages(self):
-        """Makes sure that dependencies are installed locally"""
-        sys.stdout.write('Checking dependencies...\n')
-
-        # Try to install it
-        try:
-            subprocess.check_call(self.PACKAGE_COMMAND)
-            return
-        except (OSError, subprocess.CalledProcessError):
-            sys.stderr.write('Failed to install dependencies; terminating\n')
-            sys.exit(1)
-
-    def run(self):
-        self.node()
-        self.npm()
-        self.packages()
-
-
 @build_command('run tests',
-               node_dependencies)
+               node.node_dependencies)
 class test(setuptools.command.test.test):
     def run(self):
         Command.run(self)

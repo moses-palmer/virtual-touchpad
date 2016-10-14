f508330312f9f408f6699b953cec677e614bce17
Moses Palmér <mosespalmer@gmail.com>
2016-10-14 12:52:55 +0200

    Added constant for static root

--- lib/virtualtouchpad/routes/static.py
+++ lib/virtualtouchpad/routes/static.py
@@ -28,7 +28,8 @@ from ._static import static as _static
 from . import get, HTTPResponse
 
 
-log = logging.getLogger(__name__)
+#: The root path for static resources
+ROOT = '.'
 
 #: The files, in the preferred order, to use as index files
 INDEX_FILES = (
@@ -39,12 +40,15 @@ INDEX_FILES = (
 @get('/')
 @get('/{filepath:.*}')
 async def static(headers, filepath=''):
+    path = os.path.join(ROOT, filepath)
+
     # If the resource is a directory, we try to serve the index file
-    if resource.isdir(filepath):
+    if resource.isdir(path):
         for index_file in (
-                os.path.join(filepath, index) for index in INDEX_FILES):
+                os.path.join(path, index)
+                for index in INDEX_FILES):
             if resource.exists(index_file):
                 return _static(headers, index_file)
         return HTTPResponse(status=404)
 
-    return _static(headers, filepath)
+    return _static(headers, path)
